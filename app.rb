179e05f1b98ab9dda010cdc2dd9b2b75ff403910
Allen Wu <allenwu1018@gmail.com>
2014-02-05 17:04:31 +0800

    xml

--- app.rb
+++ app.rb
@@ -3,6 +3,7 @@ $:.unshift File.expand_path("../lib", __FILE__)
 require 'sinatra'
 require 'sinatra/devise'
 require 'sinatra/reloader' if development?
+require 'multi_xml'
 
 configure do
   enable :logging
@@ -23,7 +24,11 @@ end
 post '/wechat' do
   logger.info "------POST------"
   logger.info params
-
+  logger.info "------BODY------"
+  #logger.info "REQUEST CLASS: #{request.class}" # Sinatra::Request < Rack::Request
+  #logger.info "BODY: #{request.body}" # StringIO < Data < Object
+  #logger.info "BODY.READ: #{request.body.read}" # String
+  logger.info "HASH: #{hash = MultiXml.parse(xml)['xml']}"
   authorize!
 
   logger.info "PASSED"
